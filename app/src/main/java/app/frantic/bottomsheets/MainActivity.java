8f39701e3c7d091c1d5090546d3c29c356347c8f
Kapil Maharjan <kapil@fusemachines.com>
2017-12-30 15:23:26 +0545

    Initial Commit

---
+++ app/src/main/java/app/frantic/bottomsheets/MainActivity.java
@@ -0,0 +1,94 @@
+package app.frantic.bottomsheets;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    LinearLayout llBottomSheet;
+    EditText editText;
+    TextView tv_text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        editText = findViewById(R.id.ed_text);
+        tv_text = findViewById(R.id.tv_text);
+
+        // get the bottom sheet view
+        llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+
+// init the bottom sheet behavior
+        final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+// change the state of the bottom sheet
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        // set the peek height
+//        bottomSheetBehavior.setPeekHeight(40);
+// set hideable or not
+        bottomSheetBehavior.setHideable(true);
+
+// set callback for changes
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
+
+        //open bottom sheet while presseing button
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        //open bottom sheet when typing in editext and make changes in realtime
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                tv_text.setText(charSequence+" Points");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+
+}
